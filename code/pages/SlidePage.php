eddb40e009519d6da464278f9f91b0c28f4f16e5
Jason S Irish <jsirish@DynamicJason.local>
2013-03-13 10:56:09 -0500

    added prev/next

--- code/pages/SlidePage.php
+++ code/pages/SlidePage.php
@@ -8,6 +8,23 @@ class SlidePage extends Page {
 
 class SlidePage_Controller extends Page_Controller {
 	
-	
+	// previous/next page links on detail pages and related
+	public function PrevNextPage($Mode = 'next') {
+   
+	  if($Mode == 'next'){
+	    $Where = "ParentID = ($this->ParentID) AND Sort > ($this->Sort)";
+	    $Sort = "Sort ASC";
+	  }
+	  elseif($Mode == 'prev'){
+	    $Where = "ParentID = ($this->ParentID) AND Sort < ($this->Sort)";
+	    $Sort = "Sort DESC";
+	  }
+	  else{
+	    return false;
+	  }
+	   
+	  return DataObject::get("SiteTree", $Where, $Sort, null, 1);
+	     
+	}
 	
 }
