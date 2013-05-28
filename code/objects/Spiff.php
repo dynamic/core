c7dd80acfc3fc468c77cc6509ee45f056efd6d69
Nic Horstmeier <nhorstmeier@Nics-MacBook-Pro.local>
2013-05-28 15:20:19 -0500

    Update spiff to allow for square image crop

--- code/objects/Spiff.php
+++ code/objects/Spiff.php
@@ -5,6 +5,7 @@ class Spiff extends DataObject {
 	static $db = array(
 		'Name' => 'Varchar',
 		'Headline' => 'Varchar',
+		'CropSquare' => 'Boolean',
 		'Description' => 'HTMLText'
 	);
 	
@@ -54,6 +55,7 @@ class Spiff extends DataObject {
 			new HTMLEditorField('Description')
 		));
 		$fields->addFieldsToTab('Root.Image', array(
+			new CheckboxField('CropSquare','Crop image as square'),
 			$ImageField
 		));
          
