736df3a86f3ab5105363d9f3d17feb2ca8028d90
Nic Horstmeier <nhorstmeier@Nics-MacBook-Pro.local>
2013-09-04 09:40:01 -0500

    Update _config.php remove CoreSiteTree_Controller extension call
    This call should be added to mysite/_config.php if CoreSiteTree_Controller is needed.

--- _config.php
+++ _config.php
@@ -3,7 +3,7 @@
 FulltextSearchable::enable();
 
 Object::add_extension('SiteTree', 'CoreSiteTree');
-Object::add_extension('ContentController', 'CoreSiteTree_Controller');
+//Object::add_extension('ContentController', 'CoreSiteTree_Controller');
 Object::add_extension('SiteConfig', 'CompanyConfig');
 Object::add_extension('SiteConfig', 'TemplateConfig');
 Object::add_extension('SiteConfig', 'SettingsConfig');
