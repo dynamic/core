6180c5550ef5dd28ba6bbba7473a940b0c292c9f
Jason Korth <jkorth@dynamicdoes.com>
2013-10-28 14:33:46 -0500

    update tracking calls

--- javascript/tracking.js
+++ javascript/tracking.js
@@ -44,10 +44,15 @@ $(document).ready(function() {
 		
 		/*Slider Calls */
 		
-		$(".flexslider img a").on('click', function(e) {
+		$(".flexslider a img").on('click', function(e) {
 			e.preventDefault();
 			console.log("Slider Image Click");
-			//_gaq.push(['_trackEvent', 'Footer Social Icon', 'Click', $(this).attr('href')]);
+			//_gaq.push(['_trackEvent', 'Slider Image', 'Click', $(this).attr('href')]);
+		});	
+		$(".flexslider a.flex-button").on('click', function(e) {
+			e.preventDefault();
+			console.log("slide button Click");
+			//_gaq.push(['_trackEvent', 'Slider Button', 'Click', $(this).attr('href')]);
 		});	
 
 
@@ -77,19 +82,39 @@ $(document).ready(function() {
 		$("#ChildEvent a img").on('click', function(e) {
 			e.preventDefault();
 			console.log("ChildListPage Image");
-			//_gaq.push(['_trackEvent', 'Spiff Image', 'Click', $(this).attr('href')]);
+			//_gaq.push(['_trackEvent', 'Child List Page Image', 'Click', $(this).attr('href')]);
 		});	
 
 		$("#ChildEvent h4 a").on('click', function(e) {
 			e.preventDefault();
 			console.log("ChildListPage h4");
-			//_gaq.push(['_trackEvent', 'Spiff Header Link', 'Click', $(this).attr('href')]);
+			//_gaq.push(['_trackEvent', 'Child List Page Title Link', 'Click', $(this).attr('href')]);
 		});	
 
 		$("#ChildEvent a.learnMore").on('click', function(e) {
 			e.preventDefault();
 			console.log("ChildListPage Learn More");
-			//_gaq.push(['_trackEvent', 'Footer Social Icon', 'Click', $(this).attr('href')]);
+			//_gaq.push(['_trackEvent', 'Child List Page button click', 'Click', $(this).attr('href')]);
+		});	
+
+		/*News Page Image/Header/Button-link Clicks */	
+
+		$(".blog-list a img").on('click', function(e) {
+			e.preventDefault();
+			console.log("blog Image");
+			//_gaq.push(['_trackEvent', 'Blog Holder Image', 'Click', $(this).attr('href')]);
+		});	
+
+		$(".blog-list h4 a").on('click', function(e) {
+			e.preventDefault();
+			console.log("blog h4");
+			//_gaq.push(['_trackEvent', 'Blog Holder Title Link', 'Click', $(this).attr('href')]);
+		});	
+
+		$(".blog-list a.readmore").on('click', function(e) {
+			e.preventDefault();
+			console.log("blog Learn More");
+			//_gaq.push(['_trackEvent', 'Blog Holder button click', 'Click', $(this).attr('href')]);
 		});	
 
 
