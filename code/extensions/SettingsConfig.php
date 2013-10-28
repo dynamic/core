e1b434ef1a73989dd2043c7cd3159a10ac299c59
Nic Horstmeier <nhorstmeier@Nics-MacBook-Pro.local>
2013-10-28 14:49:15 -0500

    Update move static db declarations to config.yml for better performance, Add BasicPage page type

--- code/extensions/SettingsConfig.php
+++ code/extensions/SettingsConfig.php
@@ -1,14 +1,10 @@
 <?php
 
 class SettingsConfig extends DataExtension {
-	
-	static $db = array(
-		'ShareThisPublisherID' => 'Varchar(255)'
-	);
-	
+
 	public function updateCMSFields(FieldList $fields) {
 		$fields->addFieldToTab('Root.SocialMedia', $addThisID = new TextField('ShareThisPublisherID', 'ShareThis Publisher ID'));
 		$addThisID->setRightTitle('Publisher ID to be used across the site for ShareThis icons');
 	}
-	
+
 }
---
+++ code/pages/BasicPage.php
@@ -0,0 +1,13 @@
+<?php
+
+	class BasicPage extends Page{
+
+
+
+	}
+
+	class BasicPage_Controller extends Page_Controller{
+
+
+
+	}
