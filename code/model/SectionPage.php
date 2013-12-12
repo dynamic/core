37e7f1ebbfb0ddb0d22971ca5fb0ef515e9b4333
Nic Horstmeier <nhorstmeier@Nics-MacBook-Pro.local>
2013-12-12 11:32:55 -0600

    #25 Update basic model statics to be declared in the _config.yml file

--- code/model/SectionPage.php
+++ code/model/SectionPage.php
@@ -2,12 +2,6 @@
 
 class SectionPage extends Page {
 
-	private static $singluar_name = "Section Page";
-	private static $plural_name = "Section Pages";
-	private static $description = 'Section Landing Page, uses Flexslider and Spiffs';
-
-	private static $hide_ancestor = "SectionPage";
-
 }
 
 class SectionPage_Controller extends Page_Controller {
