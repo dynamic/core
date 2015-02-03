5562d2235ede9127d2f0fe482009820bdb49b197
muskie9 <nhorstmeier@gmail.com>
2015-02-03 10:18:26 -0600

    Add initial tests with travis support

---
+++ tests/AlbumPageTest.php
@@ -0,0 +1,53 @@
+<?php
+
+class AlbumPageTest extends DC_Test{
+
+    protected static $use_draft_site = true;
+
+    function setUp(){
+        parent::setUp();
+    }
+
+    function testAlbumPageCreation(){
+
+        $this->logInWithPermission('Album_CRUD');
+        $album = singleton('AlbumPage');
+        $this->assertTrue($album->canCreate());
+        $this->logOut();
+
+    }
+
+    function testAlbumPageDeletion(){
+
+        $this->logInWithPermission('ADMIN');
+        $holder = AlbumGroupPage::create();
+        $holder->Title = 'Albums';
+        $holder->doPublish();
+        $holderID = $holder->ID;
+        $album = $this->objFromFixture('AlbumPage', 'album1');
+        $album->ParentID = $holderID;
+        $albumID = $album->ID;
+
+        $album->doPublish();
+        $this->assertTrue($album->isPublished());
+
+        $versions = DB::query('Select * FROM "SiteTree_versions" WHERE "RecordID" = '. $albumID);
+        $versionsPostPublish = array();
+        foreach($versions as $versionRow) $versionsPostPublish[] = $versionRow;
+
+        $this->logOut();
+        $this->logInWithPermission('Album_CRUD');
+        $this->assertTrue($album->canDelete());
+
+        $album->delete();
+        $this->assertTrue(!$album->isPublished());
+
+        $versions = DB::query('Select * FROM "SiteTree_versions" WHERE "RecordID" = '. $albumID);
+        $versionsPostDelete = array();
+        foreach($versions as $versionRow) $versionsPostDelete[] = $versionRow;
+
+        $this->assertTrue($versionsPostPublish == $versionsPostDelete);
+
+    }
+
+}
