6757a2240c8262aac042cf04b8affb2b19a16ecd
Jason Irish <jirish@dynamicagency.com>
2015-07-11 20:54:09 -0500

    Spiff Grid Field - only ClassName Spiff

--- code/extensions/SpiffManager.php
+++ code/extensions/SpiffManager.php
@@ -1,11 +1,11 @@
 <?php
 
 class SpiffManager extends DataExtension {
-	
+
 	static $many_many = array(
 		'Spiffs' => 'Spiff'
 	);
-	
+
 	public static $many_many_extraFields = array(
 		'Spiffs' => array(
 			'SortOrder' => 'Int'
@@ -15,10 +15,10 @@ class SpiffManager extends DataExtension {
 	public function getSpiffList() {
 		return $this->owner->Spiffs()->sort('SortOrder');
 	}
-	
+
 	public function updateCMSFields(FieldList $fields) {
-		
-		$config = GridFieldConfig_RelationEditor::create();	
+
+		$config = GridFieldConfig_RelationEditor::create();
 		//$config->addComponent(new GridFieldBulkEditingTools());
 		//$config->addComponent(new GridFieldBulkImageUpload('ImageID', array('Name')));
 		if(class_exists('GridFieldManyRelationHandler')){
@@ -31,16 +31,21 @@ class SpiffManager extends DataExtension {
             if(class_exists('GridFieldSortableRows')) $config->addComponent(new GridFieldSortableRows("SortOrder"));
 		}
 
-	    
-		$SpiffGridField = GridField::create("Spiffs", "Spiffs", $this->owner->Spiffs()->sort('SortOrder'), $config);
-	    	    
+
+		$SpiffGridField = GridField::create(
+			"Spiffs",
+			"Spiffs",
+			$this->owner->Spiffs()->filter(array('ClassName' => 'Spiff'))->sort('SortOrder'),
+			$config
+		);
+
 	    // add FlexSlider, width and height
 	    $fields->addFieldsToTab("Root.Spiffs", array(
 	    	$SpiffGridField
 	    ));
-		
+
 	}
-	
+
 	public static function SideBarSpiff(){
 		$className = Controller::curr()->Data()->ClassName;
 		if($className=='DynamicHomePage'){
@@ -52,5 +57,5 @@ class SpiffManager extends DataExtension {
 	public function getSpiffs(){
 		return $this->owner->getManyManyComponents('Spiffs')->sort('SortOrder');
 	}
-		
+
 }
