a9624564ca8f1a891463be2e5f8feda4a5b0de86
Jason Irish <jirish@dynamicagency.com>
2016-01-29 16:10:04 -0600

    BUGFIX: SiteMap - remove DataList qualifier for Children
    In line 27, `$page->Children()` returns an ArrayList. Throws an error since `getSiteMap90` previously required a DataList
    
    fixes #92
    
    also ran cs fixer for formatting

--- code/pages/SiteMap.php
+++ code/pages/SiteMap.php
@@ -1,80 +1,90 @@
 <?php
 
-class SiteMap extends Page implements PermissionProvider{
+class SiteMap extends Page implements PermissionProvider
+{
+    private static $singular_name = 'Site Map';
+    private static $plural_name = 'Site Maps';
+    private static $description = "Displays a Site Map from your site's content";
 
-	private static $singular_name = "Site Map";
-	private static $plural_name = "Site Maps";
-	private static $description = "Displays a Site Map from your site's content";
+    /**
+     * @return string
+     */
+    public function getSitemap($set = null)
+    {
+        if (!$set) {
+            $set = $this->getRootPages();
+        }
 
-	/**
-	 * @return string
-	 */
-	public function getSitemap(DataList $set = null) {
-		if(!$set) $set = $this->getRootPages();
+        if ($set && count($set)) {
+            $sitemap = '<ul>';
 
-		if($set && count($set)) {
-			$sitemap = '<ul>';
+            foreach ($set as $page) {
+                if ($page->ShowInMenus && $page->ID != $this->ID && $page->canView()) {
+                    $sitemap .= sprintf(
+                        '<li><a href="%s" title="%s">%s</a>',
+                        $page->XML_val('Link'),
+                        $page->XML_val('MenuTitle'),
+                        $page->XML_val('Title')
+                    );
 
-			foreach($set as $page) {
-				if($page->ShowInMenus && $page->ID != $this->ID && $page->canView()) {
-					$sitemap .= sprintf (
-						'<li><a href="%s" title="%s">%s</a>',
-						$page->XML_val('Link'),
-						$page->XML_val('MenuTitle'),
-						$page->XML_val('Title')
-					);
+                    if ($children = $page->Children()) {
+                        $sitemap .= $this->getSitemap($children);
+                    }
 
-					if($children = $page->Children()) {
-						$sitemap .= $this->getSitemap($children);
-					}
+                    $sitemap .= '</li>';
+                }
+            }
 
-					$sitemap .= '</li>';
-				}
-			}
+            return $sitemap.'</ul>';
+        }
+    }
 
-			return $sitemap .'</ul>';
-		}
-	}
-
-	/**
-	 * @return DataList
-	 */
-	public function getRootPages() {
-		return SiteTree::get()->filter(array("ParentID" => 0, "ShowInMenus" => 1));
-	}
+    /**
+     * @return DataList
+     */
+    public function getRootPages()
+    {
+        return SiteTree::get()->filter(array('ParentID' => 0, 'ShowInMenus' => 1));
+    }
 
     /**
      * @param Member $member
-     * @return boolean
+     *
+     * @return bool
      */
-    public function canView($member = null) {
+    public function canView($member = null)
+    {
         return parent::canView($member = null);
     }
 
-    public function canEdit($member = null) {
+    public function canEdit($member = null)
+    {
         return Permission::check('SiteMapPage_CRUD');
     }
 
-    public function canDelete($member = null) {
+    public function canDelete($member = null)
+    {
         return Permission::check('SiteMapPage_CRUD');
     }
 
-    public function canCreate($member = null) {
-        if (SiteMap::get()->first()) return false;
+    public function canCreate($member = null)
+    {
+        if (self::get()->first()) {
+            return false;
+        }
+
         return Permission::check('SiteMapPage_CRUD');
     }
 
-    public function providePermissions() {
+    public function providePermissions()
+    {
         return array(
             //'Location_VIEW' => 'Read a Location',
-            'SiteMapPage_CRUD' => 'Create, Update and Delete a Site Map Page'
+            'SiteMapPage_CRUD' => 'Create, Update and Delete a Site Map Page',
         );
     }
-
 }
 
-class SiteMap_Controller extends Page_Controller {
-
-
-
+class SiteMap_Controller extends Page_Controller
+{
 }
