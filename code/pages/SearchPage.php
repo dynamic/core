145d0752cdf08b6ed17b5976a84d7fa7e4f5f46c
Jason Irish <jirish@dynamicagency.com>
2016-01-29 16:07:49 -0600

    BUGFIX: SearchPage - allowed actions
    uncomment the existing allowed actions to fix error on search results
    
    fixes #94
    
    also ran cs fixer for formatting

--- code/pages/SearchPage.php
+++ code/pages/SearchPage.php
@@ -1,51 +1,56 @@
 <?php
 
-class SearchPage extends Page implements PermissionProvider{
+class SearchPage extends Page implements PermissionProvider
+{
+    private static $singular_name = 'Search Page';
+    private static $plural_name = 'Search Pages';
+    private static $description = 'Website search. Searches Title and Content field of each page.';
 
-	private static $singular_name = "Search Page";
-	private static $plural_name = "Search Pages";
-	private static $description = 'Website search. Searches Title and Content field of each page.';
-
-	private static $defaults = array(
-		'ShowInMenus' => 0
-	);
+    private static $defaults = array(
+        'ShowInMenus' => 0,
+    );
 
     /**
      * @param Member $member
-     * @return boolean
+     *
+     * @return bool
      */
-    public function canView($member = null) {
+    public function canView($member = null)
+    {
         return parent::canView($member = null);
     }
 
-    public function canEdit($member = null) {
+    public function canEdit($member = null)
+    {
         return Permission::check('SearchPage_CRUD');
     }
 
-    public function canDelete($member = null) {
+    public function canDelete($member = null)
+    {
         return Permission::check('SearchPage_CRUD');
     }
 
-    public function canCreate($member = null) {
-        if (SearchPage::get()->first()) return false;
+    public function canCreate($member = null)
+    {
+        if (self::get()->first()) {
+            return false;
+        }
+
         return Permission::check('SearchPage_CRUD');
     }
 
-    public function providePermissions() {
+    public function providePermissions()
+    {
         return array(
             //'Location_VIEW' => 'Read a Location',
-            'SearchPage_CRUD' => 'Create, Update and Delete a Search Page'
+            'SearchPage_CRUD' => 'Create, Update and Delete a Search Page',
         );
     }
-
 }
 
-class SearchPage_Controller extends Page_Controller {
-
-	/*
-	private static $allowed_actions = array(
-		'SearchForm'
-	);
-	*/
-
+class SearchPage_Controller extends Page_Controller
+{
+    private static $allowed_actions = array(
+        'SearchForm',
+    );
 }
