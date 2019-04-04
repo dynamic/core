b2569cb235b5df04f91483c3822d9e6f094f48a3
Jason Irish <jirish@dynamicagency.com>
2019-04-04 13:15:36 -0500

    _config.php cleanup

--- _config.php
+++ _config.php
@@ -3,16 +3,5 @@
 // enable site search
 FulltextSearchable::enable();
 
-// SPAM protection
-if (class_exists('SpamProtectorManager') && class_exists('MathSpamProtector')) {
-    SpamProtectorManager::set_spam_protector("MathSpamProtector");
-}
-
-// Addressable
-if (class_exists('Addressable')) {
-	Object::add_extension('SiteConfig', 'Addressable');
-	Object::add_extension('SiteConfig', 'Geocodable');
-}
-
 // set image upload max size
 define('CORE_IMAGE_FILE_SIZE_LIMIT', 512000);
